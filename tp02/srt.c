4265c2bf99f77d11d4310237add6924404729c11
SiIky <siiky@users.noreply.github.com>
2017-11-16 17:29:24 +0000

    Ler, processar e imprimir ficheiros de entrada

--- tp02/srt.c
+++ tp02/srt.c
@@ -5,17 +5,27 @@
 
 struct srt srt_print_sub (struct srt srt)
 {
-    printf("%s", srt.sub);
+    printf(
+        "%d\n"
+        "%02d:%02d:%02d,%03d"
+        " --> "
+        "%02d:%02d:%02d,%03d\n"
+        "%s\n",
+        srt.id,
+        srt.interval.begin.tm.tm_hour,
+        srt.interval.begin.tm.tm_min,
+        srt.interval.begin.tm.tm_sec,
+        srt.interval.begin.tm_mil,
+        srt.interval.end.tm.tm_hour,
+        srt.interval.end.tm.tm_min,
+        srt.interval.end.tm.tm_sec,
+        srt.interval.end.tm_mil,
+        srt.sub
+        );
     return srt;
 }
 
 struct srt srt_dtor (struct srt srt)
 {
-    /*
-    if (srt.sub != NULL) {
-        free(srt.sub);
-        srt.sub = NULL;
-    }
-    */
     return srt;
 }
